2cadf75e1d5a646433c62e9e55a0b9165ae4c9f6
losferwords <losfer@list.ru>
2015-11-27 13:22:50 +0300

    Test address for heroku

--- socket/index.js
+++ socket/index.js
@@ -41,11 +41,9 @@ module.exports = function (server) {
 
     var io = require('socket.io').listen(server);
 
-    io.configure(function () {
-        io.set("transports", ["xhr-polling"]);
-        io.set("polling duration", 10);
-    });
-
+    io.set("transports", ["xhr-polling"]);
+    io.set("polling duration", 10);
+    
     //io.set('origins', 'http://node.test:*');
 
     io.use(function (socket, next) {
